66eccca5762719e91eb8920e47dd761e31ee6b45
fiigmnt <fiigmnt@protonmail.com>
2024-02-28 13:12:47 -0500

    update db prod

--- utils/prisma.ts
+++ utils/prisma.ts
@@ -1,7 +1,5 @@
 // utils/prisma.js
 
-// utils/prisma.js
-
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 
@@ -12,17 +10,6 @@ export default prisma;
 
 // import { PrismaClient } from '@prisma/client';
 
-// declare const global: NodeJS.Global;
-
-// let prisma: PrismaClient;
-
-// if (process.env.NODE_ENV === 'production') {
-//   prisma = new PrismaClient();
-// } else {
-//   if (!global.prisma) {
-//     global.prisma = new PrismaClient();
-//   }
-//   prisma = global.prisma;
-// }
+// const prisma = new PrismaClient();
 
 // export default prisma;
