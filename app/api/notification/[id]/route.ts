24f7842bc6cbe1601b49110ff4eaaccf79449b7a
fiigmnt <fiigmnt@protonmail.com>
2024-03-22 16:42:57 -0400

    notification

---
+++ app/api/notification/[id]/route.ts
@@ -0,0 +1,32 @@
+// GET USER ENDPOINT
+import prisma from "@/utils/prisma";
+
+export async function GET(request: Request, { params }: { params: { id: string } }) {
+  const boxId = params.id;
+
+  console.log(boxId);
+
+  const latestBox = await prisma.box.findFirst({
+    where: {
+      collectorId: {
+        not: null,
+      },
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+    include: {
+      collector: true,
+    },
+  });
+
+  if (!latestBox) {
+    return Response.json({ success: false, message: "Box does not exist" });
+  }
+
+  if (boxId === latestBox?.id) {
+    return Response.json({ success: false, message: "No new boxes" });
+  }
+
+  return Response.json(latestBox);
+}
--- components/sponsor/notification/BoxNotification.tsx
+++ components/sponsor/notification/BoxNotification.tsx
@@ -1,33 +1,69 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useRef, useState } from "react";
 import Image from "next/image";
 
 import { withCommas } from "@/utils";
 
 import styles from "./BoxNotification.module.css";
+import { User } from "@/types";
 
 const BoxNotification: React.FC = () => {
+  const boxId = useRef<string>("0");
+  const [user, setUser] = useState<User | null>(null);
+  const [points, setPoints] = useState<number>(0);
+  const [show, setShow] = useState<boolean>(false);
+
+  const fetchLatestBox = async () => {
+    const res = await fetch(`/api/notification/${boxId.current}`);
+    const data = await res.json();
+
+    if (data.success !== false) {
+      setShow(true);
+      setTimeout(() => {
+        setShow(false);
+      }, 5000);
+      boxId.current = data.id;
+      setPoints(data.points);
+      setUser(data.collector);
+    }
+  };
+
+  useEffect(() => {
+    const getBoxNotification = setInterval(() => {
+      fetchLatestBox();
+    }, 1000);
+
+    return () => {
+      clearInterval(getBoxNotification);
+    };
+  }, []);
+
   const icon = "assets/icons/icons-24/box-opened-green.svg";
-  const points = 1000;
   return (
-    <div className={styles.container}>
-      <div className={styles.left}>
-        <div className={styles.icon}>
-          <Image src={icon} alt="User Icon" width={32} height={32} />
+    show && (
+      <>
+        <div className={styles.container}>
+          <div className={styles.left}>
+            <div className={styles.icon}>
+              <Image src={user?.image || icon} alt="User Icon" width={32} height={32} />
+            </div>
+            <Image src="/assets/icons/icons-24/box-opened-green.svg" alt="Box Opened" width={24} height={24} />
+            <p>Claimed!</p>
+          </div>
+
+          <div className={styles.right}>
+            <div className={styles.points}>{withCommas(points)}</div>
+
+            <Image src="/assets/icons/icons-24/g-points.svg" alt="Coin Icon" width={24} height={24} />
+
+            <div className={styles.pin}>
+              <Image src="/assets/icons/icons-24/pin.svg" alt="Pin" width={24} height={24} />
+            </div>
+          </div>
         </div>
-        <Image src="/assets/icons/icons-24/box-opened-green.svg" alt="Box Opened" width={24} height={24} />
-        <p>Claimed!</p>
-      </div>
-
-      <div className={styles.right}>
-        <div className={styles.points}>{withCommas(points)}</div>
-
-        <Image src="/assets/icons/icons-24/g-points.svg" alt="Coin Icon" width={24} height={24} />
-
-        <div className={styles.pin}>
-          <Image src="/assets/icons/icons-24/pin.svg" alt="Pin" width={24} height={24} />
-        </div>
-      </div>
-    </div>
+      </>
+    )
   );
 };
 
