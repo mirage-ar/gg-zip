bb611ff9f13e92dc17fa62e9347a70889dfac2ad
fiigmnt <fiigmnt@protonmail.com>
2024-03-23 12:00:14 -0400

    live

--- components/utility/Timer.tsx
+++ components/utility/Timer.tsx
@@ -39,9 +39,10 @@ const Timer: React.FC<TimerProps> = ({ date }) => {
         }
         const seconds = Math.floor((time / 1000) % 60).toString().padStart(2, '0');
         const minutes = Math.floor((time / 1000 / 60) % 60).toString().padStart(2, '0');
-        const hours = Math.floor((time / (1000 * 60 * 60))).toString().padStart(2, '0');
+        const hours = Math.floor((time / (1000 * 60 * 60)) % 24).toString().padStart(2, '0');
+        const days = Math.floor(time / (1000 * 60 * 60 * 24)).toString();
 
-        return `${hours}:${minutes}:${seconds}`;
+        return `${days}:${hours}:${minutes}:${seconds}`;
     };
 
     return <div className={styles.main}>{formatTime(timeRemaining)}</div>;
