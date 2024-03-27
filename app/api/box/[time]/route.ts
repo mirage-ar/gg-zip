9ccbf61f4be7a508a2d058a3e5723e2312ada67a
fiigmnt <fiigmnt@protonmail.com>
2024-03-27 14:24:11 -0400

    testing

--- app/api/box/[time]/route.ts
+++ app/api/box/[time]/route.ts
@@ -5,28 +5,28 @@ export async function GET(request: Request) {
   try {
     // get all boxes within a certain area
     const boxes = await prisma.box.findMany();
-    const features = boxes.map((box) => {
-      return {
-        type: "Feature",
-        properties: {
-          id: box.id,
-          boxType: box.collectorId ? "opened" : "closed",
-        },
-        geometry: {
-          type: "Point",
-          coordinates: [box.longitude, box.latitude],
-        },
-      };
-    });
+    // const features = boxes.map((box) => {
+    //   return {
+    //     type: "Feature",
+    //     properties: {
+    //       id: box.id,
+    //       boxType: box.collectorId ? "opened" : "closed",
+    //     },
+    //     geometry: {
+    //       type: "Point",
+    //       coordinates: [box.longitude, box.latitude],
+    //     },
+    //   };
+    // });
 
-    const geoJSON = {
-      type: "FeatureCollection",
-      features,
-    };
+    // const geoJSON = {
+    //   type: "FeatureCollection",
+    //   features,
+    // };
 
     return Response.json({
       collect: null,
-      boxes: geoJSON,
+      boxes: [],
     });
   } catch (error) {
     throw new Error(`BOXES ERROR: ${error}`);
