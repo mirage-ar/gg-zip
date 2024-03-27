0be6b2807be286d07ed107101923afc586e86eb9
fiigmnt <fiigmnt@protonmail.com>
2024-03-27 14:56:52 -0400

    enable cache

--- app/api/box/[time]/route.ts
+++ app/api/box/[time]/route.ts
@@ -3,26 +3,31 @@ import prisma from "@/utils/prisma";
 
 export async function GET(request: Request) {
   try {
-    // get all boxes within a certain area
-    const boxes = await prisma.box.findMany();
-    // const features = boxes.map((box) => {
-    //   return {
-    //     type: "Feature",
-    //     properties: {
-    //       id: box.id,
-    //       boxType: box.collectorId ? "opened" : "closed",
-    //     },
-    //     geometry: {
-    //       type: "Point",
-    //       coordinates: [box.longitude, box.latitude],
-    //     },
-    //   };
-    // });
 
-    // const geoJSON = {
-    //   type: "FeatureCollection",
-    //   features,
-    // };
+    const boxes = await prisma.box.findMany({
+      cacheStrategy: {
+        ttl: 5,
+      },
+    });
+
+    const features = boxes.map((box) => {
+      return {
+        type: "Feature",
+        properties: {
+          id: box.id,
+          boxType: box.collectorId ? "opened" : "closed",
+        },
+        geometry: {
+          type: "Point",
+          coordinates: [box.longitude, box.latitude],
+        },
+      };
+    });
+
+    const geoJSON = {
+      type: "FeatureCollection",
+      features,
+    };
 
     return Response.json({
       collect: null,
