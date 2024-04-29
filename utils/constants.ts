1d64839d852b37e536bf0566aa3ae01696c2afb6
fiigmnt <fiigmnt@protonmail.com>
2024-04-29 15:06:54 -0400

    bump

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,9 +1,9 @@
 // CONSTANTS
-export const LOCATION_SOCKET_URL = "wss://nty9q3etgg.execute-api.us-east-1.amazonaws.com/dev";
-export const CHAT_SOCKET_URL = "wss://zakk39uxqa.execute-api.us-east-1.amazonaws.com/devs";
+export const LOCATION_SOCKET_URL = "wss://kyq7tl6nkg.execute-api.us-east-1.amazonaws.com/dev";
+export const CHAT_SOCKET_URL = "wss://2lutpaxvma.execute-api.us-east-1.amazonaws.com/dev";
 export const GET_MESSAGES_URL = "https://messages.koji.im";
 
-export const API = "https://api2.koji.im";
+export const API = "https://zzzvktfgb6.execute-api.us-east-1.amazonaws.com";
 
 export const GAME_DATE = {
   year: 2024,
