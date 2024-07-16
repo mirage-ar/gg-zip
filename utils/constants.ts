1a28e5e2c95796bbae7874322d1a7d58abdef8b0
fiigmnt <fiigmnt@protonmail.com>
2024-07-16 10:08:35 -0400

    timer

--- utils/constants.ts
+++ utils/constants.ts
@@ -15,11 +15,11 @@ export const POLLING_TIME = 5000;
 export const GAME_DATE = {
   year: 2024,
   month: 7,
-  day: 4,
+  day: 17,
 };
 
 export const GAME_TIME = 10;
 export const GAME_LENGTH = 60 * 60 * 1000;
 export const PLAYER_COUNT = 150;
 
-export const PAUSE = true;
+export const PAUSE = false;
