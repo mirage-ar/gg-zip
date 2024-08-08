42a9bedc3e11fd0156a518b9c42eea8e24a0f747
fiigmnt <fiigmnt@protonmail.com>
2024-08-07 22:21:05 -0400

    helius

--- utils/constants.ts
+++ utils/constants.ts
@@ -8,10 +8,10 @@ export const GAME_API = "https://api.koji.im";
 
 // export const RPC = "https://solana-mainnet.g.alchemy.com/v2/Sl-oMviUvRv1ztohIeigvqa7j7aOkZEz";
 
-// export const RPC = "https://mainnet.helius-rpc.com/?api-key=ea86c080-5223-4bbb-a21a-b610c509b119";
+export const RPC = "https://mainnet.helius-rpc.com/?api-key=ea86c080-5223-4bbb-a21a-b610c509b119";
 
 // DEVNET
-export const RPC = "https://solana-devnet.g.alchemy.com/v2/Sl-oMviUvRv1ztohIeigvqa7j7aOkZEz";
+// export const RPC = "https://solana-devnet.g.alchemy.com/v2/Sl-oMviUvRv1ztohIeigvqa7j7aOkZEz";
 
 export const PROGRAM_ID = "FeLN737SaQbjDAE7TfTP5QcgmS9zqeoEcYrU4umwZxxb";
 
