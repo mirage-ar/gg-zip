a93f7111793bfd17fddfef0d89a80dbdea7a3ada
fiigmnt <fiigmnt@protonmail.com>
2024-08-19 17:04:58 -0400

    hunter bug

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -23,7 +23,7 @@ const OPTIONS = {
 
   callbacks: {
     async redirect({ url, baseUrl }: any) {
-      return baseUrl + "/mint";
+      return baseUrl + "/hunter";
     },
 
     async jwt({ token, user }: any) {
